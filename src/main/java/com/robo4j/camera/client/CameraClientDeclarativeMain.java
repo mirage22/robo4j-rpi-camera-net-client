596c3d152a27f44639e13416eb71947e4711c891
Miro Wengner <miro@robo4j.io>
2018-10-14 21:39:57 +0200

    initial commit

---
+++ src/main/java/com/robo4j/camera/client/CameraClientDeclarativeMain.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2014, 2017, Marcus Hirt, Miroslav Wengner
+ *
+ * Robo4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Robo4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.robo4j.camera.client;
+
+import com.robo4j.RoboBuilder;
+import com.robo4j.RoboContext;
+import com.robo4j.RoboReference;
+import com.robo4j.units.rpi.camera.RaspistillRequest;
+import com.robo4j.units.rpi.camera.RpiCameraProperty;
+import com.robo4j.util.SystemUtil;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * CameraClientDeclarativeMain configures and run the system
+ *
+ * @author Marcus Hirt (@hirt)
+ * @author Miro Wengner (@miragemiko)
+ */
+public class CameraClientDeclarativeMain {
+
+    public static void main(String[] args) throws Exception {
+
+        InputStream systemIS;
+        InputStream contextIS;
+
+        switch (args.length) {
+            case 0:
+                systemIS = Thread.currentThread().getContextClassLoader().getResourceAsStream("robo4jSystem.xml");
+                contextIS = Thread.currentThread().getContextClassLoader().getResourceAsStream("robo4j.xml");
+                System.out.println("Default configuration used");
+                break;
+            case 1:
+                systemIS = Thread.currentThread().getContextClassLoader().getResourceAsStream("robo4jSystem.xml");
+                Path contextPath = Paths.get(args[0]);
+                contextIS = Files.newInputStream(contextPath);
+                System.out.println("Robo4j config file has been used: " + args[0]);
+                break;
+            case 2:
+                Path systemPath2 = Paths.get(args[0]);
+                Path contextPath2 = Paths.get(args[1]);
+                systemIS = Files.newInputStream(systemPath2);
+                contextIS = Files.newInputStream(contextPath2);
+                System.out.println(String.format("Custom configuration used system: %s, context: %s",args[0], args[1]));
+                break;
+            default:
+                System.out.println("Could not find the *.xml settings for the CameraClient!");
+                System.out.println("java -jar camera.jar system.xml context.xml");
+                System.exit(2);
+                throw new IllegalStateException("see configuration");
+        }
+
+        RoboBuilder builder = new RoboBuilder(systemIS);
+        builder.add(contextIS);
+        RoboContext system = builder.build();
+        system.start();
+
+        System.out.println("State after start:");
+        System.out.println(SystemUtil.printStateReport(system));
+
+        //@formatter:off
+		final RaspistillRequest activeRequest = new RaspistillRequest(true)
+				.put(RpiCameraProperty.WIDTH, "640")
+				.put(RpiCameraProperty.HEIGHT, "480")
+                .put(RpiCameraProperty.TIMEOUT, "1")
+                .put(RpiCameraProperty.EXPORSURE, "sports")
+				.put(RpiCameraProperty.TIMELAPSE, "50")
+                .put(RpiCameraProperty.ENCODING, "jpg")
+				.put(RpiCameraProperty.NOPREVIEW, "")
+				.put(RpiCameraProperty.OUTPUT, "-");
+		//@formatter:on
+
+        RoboReference<RaspistillRequest> cameraUnit = system.getReference("camera");
+        cameraUnit.sendMessage(activeRequest);
+
+        System.out.println("Press enter to quit!");
+        System.in.read();
+        system.shutdown();
+
+    }
+}
---
+++ src/main/java/com/robo4j/camera/client/unit/CameraConfigurationUnit.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2014, 2017, Marcus Hirt, Miroslav Wengner
+ *
+ * Robo4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Robo4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.robo4j.camera.client.unit;
+
+import com.robo4j.ConfigurationException;
+import com.robo4j.RoboContext;
+import com.robo4j.RoboUnit;
+import com.robo4j.configuration.Configuration;
+import com.robo4j.socket.http.codec.CameraConfigMessage;
+import com.robo4j.units.rpi.camera.RaspistillRequest;
+import com.robo4j.units.rpi.camera.RpiCameraProperty;
+
+/**
+ * CameraConfigurationUnit configures RaspistillUnit
+ *
+ * @author Marcus Hirt (@hirt)
+ * @author Miroslav Wengner (@miragemiko)
+ */
+public class CameraConfigurationUnit extends RoboUnit<CameraConfigMessage> {
+
+    public static final String NAME = "cameraConfigurationUnit";
+    private static final String PROPERTY_TARGET = "target";
+    private String target;
+
+    public CameraConfigurationUnit(RoboContext context, String id) {
+        super(CameraConfigMessage.class, context, id);
+    }
+
+
+    @Override
+    protected void onInitialization(Configuration configuration) throws ConfigurationException {
+        target = configuration.getString(PROPERTY_TARGET, null);
+        if (target == null) {
+            throw ConfigurationException.createMissingConfigNameException(PROPERTY_TARGET);
+        }
+    }
+
+    @Override
+    public void onMessage(CameraConfigMessage message) {
+        //@formatter:off
+		final RaspistillRequest cameraRequest = new RaspistillRequest(true)
+				.put(RpiCameraProperty.WIDTH, message.getWidth().toString())
+				.put(RpiCameraProperty.HEIGHT, message.getHeight().toString())
+                .put(RpiCameraProperty.BRIGHTNESS, message.getBrightness().toString())
+                .put(RpiCameraProperty.SHARPNESS, message.getSharpness().toString())
+				.put(RpiCameraProperty.TIMELAPSE, message.getTimelapse().toString())
+                .put(RpiCameraProperty.TIMEOUT, "1")
+                .put(RpiCameraProperty.EXPORSURE, "sports")
+                .put(RpiCameraProperty.ENCODING, "jpg")
+				.put(RpiCameraProperty.NOPREVIEW, "")
+				.put(RpiCameraProperty.OUTPUT, "-");
+		//@formatter:on
+        getContext().getReference(target).sendMessage(cameraRequest);
+    }
+}
